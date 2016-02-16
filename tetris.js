e35acbd25c2d2be56c3d8f74af6b9a2dd1f80646
mushymush <annafjakobsen@gmail.com>
2016-02-16 21:54:46 +0100

    added pinkBox_x and keypress

--- tetris.js
+++ tetris.js
@@ -1,12 +1,18 @@
 var pinkBox_y = 0;
 
-var blueBox_y = 10
+var pinkBox_x = 0;
+
+var blueBox_y = 10;
 
 var gameLoop = function(){
 	
 	if (pinkBox_y <= 80) {
 		pinkBox_y = pinkBox_y + 10;
 	}
+	//if (pinkBox_x <= 10) {
+	//	pinkBox_x = pinkBox_x + 10;
+	//}
+
 	if (blueBox_y <= 80) {
 		blueBox_y = blueBox_y + 10;
 	}
@@ -14,16 +20,17 @@ var gameLoop = function(){
 	ctx.fillStyle = "#9E969E";
 	ctx.fillRect(0,0,100,100);
 	ctx.fillStyle = "#EC7DF0";
-	ctx.fillRect(0,pinkBox_y,10,10);
+	ctx.fillRect(pinkBox_x,pinkBox_y,10,10);
 
 	ctx.fillStyle = "#4DC6EB";
 	ctx.fillRect(30,blueBox_y,10,10);
 
 }
 
-var keypress = function(e) {
-	console.log(e);
-
+var keypress = function() {
+	if (pinkBox_y < 90) {
+		pinkBox_x = pinkBox_x + 10;
+	}
 }
 
 window.setInterval(gameLoop, 1000) 
